3b0784f3723ba2c8425300a1ca5822237c61f800
metes12 <metes@naver.com>
2018-11-11 14:22:09 +0900

    initial commit

---
+++ app/src/main/java/com/soullotto/LottoMainActivity.java
@@ -0,0 +1,15 @@
+package com.soullotto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.soullotto.soullotto.R;
+
+public class LottoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lotto_main);
+    }
+}
